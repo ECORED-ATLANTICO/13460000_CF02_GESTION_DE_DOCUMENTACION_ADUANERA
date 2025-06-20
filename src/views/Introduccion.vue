4b0e961085e1d519324c345c1e2e646cc5dd337b
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-06-20 14:48:09 -0500

    video agregado

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -21,8 +21,7 @@
     
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/w8Q9hokKR74" title="Gestión de documentación aduanera" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 </template>
 
 <script>
